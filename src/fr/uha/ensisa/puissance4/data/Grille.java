8186290a66d8a8227ad7e0c3c056159066c4991a
Linkdu40 <lucas.gauziede@hotmail.fr>
2018-05-04 04:35:33 +0200

    Fonction d'évaluation + ajout de noms aux IA

--- src/fr/uha/ensisa/puissance4/data/Grille.java
+++ src/fr/uha/ensisa/puissance4/data/Grille.java
@@ -1,5 +1,7 @@
 package fr.uha.ensisa.puissance4.data;
 
+import java.util.ArrayList;
+
 import fr.uha.ensisa.puissance4.util.Constantes;
 import fr.uha.ensisa.puissance4.util.Constantes.Case;
 
@@ -151,20 +153,17 @@ public class Grille {
 	 * @return
 	 */
 	public double evaluer(Case symboleJoueurCourant) {
-		double valeurJoueurCourant = 0;
-
-		double valeurH = evaluerHorizontalement(symboleJoueurCourant);
-		if (valeurH == Integer.MAX_VALUE)
-			return valeurH;
-		double valeurV = evaluerVerticalement(symboleJoueurCourant);
-		if (valeurV == Integer.MAX_VALUE)
-			return valeurV;
-		double valeurD = evaluerDiagonales(symboleJoueurCourant);
-		if (valeurD == Integer.MAX_VALUE)
-			return valeurD;
-
-		valeurJoueurCourant = valeurH + valeurV + valeurD;
-		return valeurJoueurCourant;
+		double valeur_lignes = evalLignes(symboleJoueurCourant);
+		if(valeur_lignes == Integer.MAX_VALUE)
+			return valeur_lignes;
+		double valeur_colonnes = evalColonnes(symboleJoueurCourant);
+		if(valeur_colonnes == Integer.MAX_VALUE)
+			return valeur_colonnes;
+		double valeur_diagonales = evalDiagonales(symboleJoueurCourant);
+		if(valeur_diagonales == Integer.MAX_VALUE)
+			return valeur_diagonales;
+		
+		return valeur_lignes+valeur_colonnes+valeur_diagonales;
 	}
 
 	/**
@@ -174,102 +173,152 @@ public class Grille {
 		Grille copy = new Grille(this);
 		return copy;
 	}
-
-	private double evaluerHorizontalement(Case symboleJoueurCourant) {
-		double valeurHorizontale = 0;
-		int nbAlignes = 0;
+	/**
+	 * Evalue la valeur de toutes les lignes dans la grille actuelle pour un joueur spécifique
+	 * @param symboleJoueurCourant
+	 * @return
+	 */
+	public double evalLignes(Case symboleJoueurCourant) {
+		double valeur_lignes = 0;
 		for (int i = 0; i < Constantes.NB_LIGNES; i++) {
+			ArrayList<Case> cellules = new ArrayList<Case>();
 			for (int j = 0; j < Constantes.NB_COLONNES; j++) {
-				if (grille[j][i] == symboleJoueurCourant)
-					nbAlignes++;
-				else {
-					if (nbAlignes > 1) {
-						valeurHorizontale += nbAlignes;
-						nbAlignes = 0;
-					}
-				}
-				// si on a gagné sur cet état on sort de la méthode et on
-				// retourne un max
-				if (nbAlignes == 4) {
-					return Integer.MAX_VALUE;
-				}
+				Case cellule = grille[j][i];
+				cellules.add(cellule);
 			}
-			if (nbAlignes > 1)
-				valeurHorizontale += nbAlignes;
-			nbAlignes = 0;
+			double valeur_ligne = valeurAlignement(cellules, symboleJoueurCourant);
+			if (valeur_ligne == Integer.MAX_VALUE) {
+				valeur_lignes = Integer.MAX_VALUE;
+				break;
+			}
+			valeur_lignes += valeur_ligne;
+			valeur_ligne = 0;
 		}
-		return valeurHorizontale;
+		return valeur_lignes;
 	}
-
-	private double evaluerVerticalement(Case symboleJoueurCourant) {
-		double valeurVerticale = 0;
-		int nbAlignes = 0;
-
-		for (int j = 0; j < Constantes.NB_COLONNES; j++) {
-			for (int i = 0; i < Constantes.NB_LIGNES; i++) {
-				if (grille[j][i] == symboleJoueurCourant)
-					nbAlignes++;
-				else {
-					if (nbAlignes > 1)
-						valeurVerticale += nbAlignes;
-					nbAlignes = 0;
-				}
-				// si on a gagné sur cet état on sort de la méthode et on
-				// retourne un max
-				if (nbAlignes == 4)
-					return Integer.MAX_VALUE;
+	/**
+	 * Evalue la valeur de toutes les colonnes dans la grille actuelle pour un joueur spécifique
+	 * @param symboleJoueurCourant
+	 * @return
+	 */
+	public double evalColonnes(Case symboleJoueurCourant) {
+		double valeur_colonnes = 0;
+		for (int i = 0; i < Constantes.NB_COLONNES; i++) {
+			ArrayList<Case> cellules = new ArrayList<Case>();
+			for (int j = 0; j < Constantes.NB_LIGNES; j++) {
+				Case cellule = grille[i][j];
+				cellules.add(cellule);
 			}
-
-			if (nbAlignes > 1)
-				valeurVerticale += nbAlignes;
-			nbAlignes = 0;
+			double valeur_colonne = valeurAlignement(cellules, symboleJoueurCourant);
+			if (valeur_colonne == Integer.MAX_VALUE) {
+				valeur_colonnes = Integer.MAX_VALUE;
+				break;
+			}
+			valeur_colonnes += valeur_colonne;
+			valeur_colonne = 0;
 		}
-		return valeurVerticale;
+		return valeur_colonnes;
 	}
+	/**
+	 * Evalue la valeur de toutes les diagonales dans la grille actuelle pour un joueur spécifique
+	 * @param symboleJoueurCourant
+	 * @return
+	 */
+	public double evalDiagonales(Case symboleJoueurCourant) {
+		double valeur_Diagonales = 0;
 
-	private double evaluerDiagonales(Case symboleJoueurCourant) {
-		double valeurDiagonale = 0;
-		int nbAlignes = 0;
-		// calcul alignements diagonaux (bas-droite vers haut-gauche)
-		for (int i = 0; i < Constantes.NB_LIGNES - 3; i++)
-			for (int j = 0; j < Constantes.NB_COLONNES - 3; j++) {
-				for (int x = 0; i + x < Constantes.NB_LIGNES && j + x < Constantes.NB_COLONNES; x++) {
-					if (grille[j + x][i + x] == symboleJoueurCourant)
-						nbAlignes++;
-					else {
-						if (nbAlignes > 1)
-							valeurDiagonale += nbAlignes;
-						nbAlignes = 0;
-					}
-					if (nbAlignes == 4) {
-						return Integer.MAX_VALUE;
-					}
-				}
-				if (nbAlignes > 1)
-					valeurDiagonale += nbAlignes;
-				nbAlignes = 0;
+		// Vérification alignement diagonaux (bas-droite vers haut-gauche)
+		for (int i = Constantes.NB_LIGNES - 3; i >= 0; i--) {
+			ArrayList<Case> cellules = new ArrayList<Case>();
+			for (int x = 0; i + x < Constantes.NB_LIGNES; x++) {
+				Case cellule = grille[x][i + x];
+				cellules.add(cellule);
 			}
-		// calcul alignements diagonaux (bas-gauche vers haut-droit)
-		for (int i = 0; i < Constantes.NB_LIGNES - 3; i++)
-			for (int j = Constantes.NB_COLONNES - 1; j >= 3; j--) {
-				for (int x = 0; i + x < Constantes.NB_LIGNES && j - x >= 0; x++) {
-					if (grille[j - x][i + x] == symboleJoueurCourant)
-						nbAlignes++;
-					else {
-						if (nbAlignes > 1)
-							valeurDiagonale += nbAlignes;
-						nbAlignes = 0;
-					}
-					if (nbAlignes == 4) {
-						return Integer.MAX_VALUE;
-					}
-				}
-				if (nbAlignes > 1)
-					valeurDiagonale += nbAlignes;
-				nbAlignes = 0;
-			}
-		return valeurDiagonale;
+			double valeur_Diagonale = valeurAlignement(cellules, symboleJoueurCourant);
+			if (valeur_Diagonale == Integer.MAX_VALUE)
+				return Integer.MAX_VALUE;
+			valeur_Diagonales += valeur_Diagonale;
+		}
 
+		for (int j = 1; j < Constantes.NB_COLONNES - 3; j++) {
+			ArrayList<Case> cellules = new ArrayList<Case>();
+			for (int y = 0; y + j < Constantes.NB_COLONNES; y++) {
+				Case cellule = grille[j + y][y];
+				cellules.add(cellule);
+			}
+			double valeur_Diagonale = valeurAlignement(cellules, symboleJoueurCourant);
+			if (valeur_Diagonale == Integer.MAX_VALUE)
+				return Integer.MAX_VALUE;
+			valeur_Diagonales += valeur_Diagonale;
+		}
+
+		// Vérification alignement diagonaux (bas-gauche vers haut-droit)
+		for (int i = Constantes.NB_LIGNES - 3; i >= 0; i--) {
+			ArrayList<Case> cellules = new ArrayList<Case>();
+			for (int x = 0; i + x < Constantes.NB_LIGNES; x++) {
+				Case cellule = grille[i][i + x];
+				cellules.add(cellule);
+			}
+			double valeur_Diagonale = valeurAlignement(cellules, symboleJoueurCourant);
+			if (valeur_Diagonale == Integer.MAX_VALUE)
+				return Integer.MAX_VALUE;
+			valeur_Diagonales += valeur_Diagonale;
+		}
+
+		for (int j = Constantes.NB_COLONNES - 1; j >= 3; j--) {
+			ArrayList<Case> cellules = new ArrayList<Case>();
+			for (int y = 0; y < j; y++) {
+				Case cellule = grille[j - y][y];
+				cellules.add(cellule);
+			}
+			double valeur_Diagonale = valeurAlignement(cellules, symboleJoueurCourant);
+			if (valeur_Diagonale == Integer.MAX_VALUE)
+				return Integer.MAX_VALUE;
+			valeur_Diagonales += valeur_Diagonale;
+		}
+		return valeur_Diagonales;
+	}
+	/**
+	 * Attribue une valeur à un enchaînement de Tokens (contenus dans une arraylist)
+	 * @param cellules
+	 * @param symboleJoueurCourant
+	 * @return
+	 */
+	public double valeurAlignement(ArrayList<Case> cellules, Case symboleJoueurCourant) {
+		double valeur_Alignement = 0;
+		int cellules_disponibles = 0;
+		int NBtokenAdj = 0;
+		int NBtokenAlignes = 0;
+		for (Case cellule : cellules) {
+			if (cellule == Case.V) {
+				cellules_disponibles++;
+				NBtokenAdj = 0;
+			} else if (cellule == symboleJoueurCourant) {
+				if (cellules_disponibles > 3)
+					valeur_Alignement += 3;
+				else
+					valeur_Alignement += cellules_disponibles;
+				cellules_disponibles = 0;
+				NBtokenAdj++;
+				if (NBtokenAdj >= 4)
+					return Integer.MAX_VALUE;
+				NBtokenAlignes++;
+			} else {
+				if (cellules_disponibles + NBtokenAlignes >= 4)
+					valeur_Alignement += cellules_disponibles + Math.pow(NBtokenAlignes, 2);
+				NBtokenAlignes = 0;
+				cellules_disponibles = 0;
+				NBtokenAdj = 0;
+			}
+		}
+		if (cellules_disponibles + NBtokenAlignes >= 4) {
+			if (cellules_disponibles < 4)
+				valeur_Alignement += cellules_disponibles + Math.pow(NBtokenAlignes, 2);
+			else
+				valeur_Alignement += 3 + Math.pow(NBtokenAlignes, 2);
+		}
+
+		return valeur_Alignement;
 	}
 
 }
